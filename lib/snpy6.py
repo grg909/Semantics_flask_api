6e545b27196782d3a85efbbfe922bec93799b39d
grg909 <grg909@foxmail.com>
2019-04-09 18:01:23 +0800

    基础功能重构

--- lib/snpy6.py
+++ lib/snpy6.py
@@ -30,12 +30,12 @@ class GraphJson:
         :param data: 数据pandas.dataframe, 有两列，第一列类别，第二列描述
         :param class_column: 定义的类别列名
         :param description_column: 定义的描述列名
-        :param scale: 1-10，1代表1k级别的数据，2代表10k
         """
         self.data = data
         self.class_column = class_column
         self.description_column = description_column
-        self.scale = 1000
+        self._repeat_class_word = set()
+        self._class_name = []
 
     def _iter_segment(self, flags):
         """
@@ -52,18 +52,30 @@ class GraphJson:
         # jieba.enable_parallel()
 
         for row in self.data.itertuples(index=False):
+            seg_list = []
             description_seg = pseg.cut(row[1].replace('\r\n', ''))
             words_seg = [word for word, flag in description_seg if flag in flags and word != row[0]]
-            seg_line = row[0]+' '+' '.join(words_seg)
-            yield seg_line
+            seg_list.append(row[0])
+            seg_list.extend(words_seg)
+            yield seg_list
 
     @staticmethod
-    def _iter_remove_stopwords(seg_line, stopwords_relative_pos):
+    def _iter_remove_stopwords(seg_list, stopwords_line):
         """
         读取停用词列表，去除输入分词字符串中的停用词。生成器版本
         :param seg_line: 分词结果字符串
-        :param stopwords_relative_pos: 停用词相对路径位置
+        :param stopwords_line: 停用词列表
         :return: 返回一个可以输出每行无停用词结果（字符串）的迭代器
+        """
+        without_stopwords = [word for word in seg_list if word not in stopwords_line]
+        yield without_stopwords
+
+    def seg_and_rm_stopwords(self, seg_flags, stopwords_relative_pos):
+        """
+        一步执行分词和去除停用词
+        :param seg_flags: 指定保留的分词flags列表
+        :param stopwords_relative_pos: 停用词相对路径位置
+        :return: 返回一个列表，每项为字符串，每个字符串为类别词加分词结果，用‘ ’连接
         """
         try:
             with open(stopwords_relative_pos, encoding='utf-8') as sp:
@@ -73,21 +85,9 @@ class GraphJson:
             print('请确保停用词库在正确目录下')
             exit()
 
-        seg_list = seg_line.split(' ')
-        without_stopwords = [word for word in seg_list if word not in stopwords_line]
-        removed_line = ' '.join(without_stopwords)
-        yield removed_line
-
-    def seg_and_rm_stopwords(self, seg_flags, stopwords_relative_pos):
-        """
-        一步执行分词和去除停用词
-        :param seg_flags: 指定保留的分词flags列表
-        :param stopwords_relative_pos: 停用词相对路径位置
-        :return: 返回一个列表，每项为字符串，每个字符串为类别词加分词结果，用‘ ’连接
-        """
         with_class_data = []
         for seg in self._iter_segment(seg_flags):
-            for seg_rm in self._iter_remove_stopwords(seg, stopwords_relative_pos):
+            for seg_rm in self._iter_remove_stopwords(seg, stopwords_line):
                 with_class_data.append(seg_rm)
 
         return with_class_data
@@ -111,8 +111,7 @@ class GraphJson:
         """
         without_class_list = []
         for line in with_class_list:
-            with_class_word = line.split(' ')
-            without_class_list.append(with_class_word[1:])
+            without_class_list.append(line[1:])
 
         data = list(itertools.chain(*without_class_list))
         word_pool = {word: count for word, count in collections.Counter(data).items() if count > int(word_threshold)}
@@ -120,7 +119,7 @@ class GraphJson:
 
     def gen_total_dict(self, with_class_list, word_threshold, class_threshold):
         """
-        生成所有出现词的词频字典
+        生成所有出现词的词频字典，若分词中出现和别的类别相同名称，则用‘_'分隔每个字重命名
         :param with_class_list: 描述分词和去停用词后得到的字符串列表
         :param word_threshold: 分词频率阈值
         :param class_threshold: 类别频率阈值
@@ -131,51 +130,51 @@ class GraphJson:
 
         class_set = set(class_dict.keys())
         word_set = set(word_dict.keys())
-        for repeat_name in class_set & word_set:
+        self._repeat_class_word = class_set & word_set
+        for repeat_name in self._repeat_class_word:
             word_dict['_'.join(repeat_name)] = word_dict.pop(repeat_name)
+
+        self._class_name = class_dict.keys()
         total_dict = {**word_dict, **class_dict}
         return total_dict
 
-    @staticmethod
-    def gen_class_word_pool(with_class_list, total_dict):
+    def gen_class_word_pool(self, with_class_list, total_dict):
         """
         生成类与对应出现过的词列表的字典
         :param with_class_list: 描述分词和去停用词后得到的字符串列表
         :param total_dict: 所有出现词的词频字典
         :return: 类与对应出现过的词列表的字典
         """
-        seen = set()
-        for item in total_dict.keys():
-            if item not in seen:
-                seen.add(item)
 
-        class_word_pool = {}
+        total_word = total_dict.keys()
+        class_word_pool = {class_name: set() for class_name in self._class_name}
 
-        for row in with_class_list:
-            line = row.split(' ')
-            if line[0] in seen:
+        for line in with_class_list:
+            if line[0] in self._class_name:
                 for word in line[1:]:
-                    if word in seen:
-                        class_word_pool.setdefault(line[0], [])
-                        if word not in class_word_pool[line[0]]:
-                            class_word_pool[line[0]].append(word)
+                    if word in total_word:
+                        if word in self._repeat_class_word:
+                            class_word_pool[line[0]].add('_'.join(word))
+                        else:
+                            class_word_pool[line[0]].add(word)
 
         return class_word_pool
 
-    def normalize(self, x, scale):
+    def _normalize(self, x, scale):
         """
-        根据数据规模不同，对分词词频进行不同程度的标准化，使得其具有相同的尺度
+        对词频进行标准化，使其具有相同的尺度([0,1]区间)
         :param x:
+        :param scale: 数据标准差
         :return:
         """
         if scale < 30:
             return 5*(2 ^ x)
         if scale > 150:
-            return x / math.log(x)
+            return x / math.log(10*x)
         else:
-            return 1.5*math.atan(x)*x/math.pi
+            return math.atan(x)*2/math.pi
 
-    def export_json_echarts(self, class_word_pool, total_dict):
+    def export_json(self, class_word_pool, total_dict):
         """
         输出图数据的json格式
         :param class_word_pool: 类与对应出现过的词列表的字典
@@ -187,8 +186,8 @@ class GraphJson:
         print('标准值：', scale)
 
         graph_nodes = total_dict.keys()  # node名称list
-        graph_nodes_sizes = list(map(lambda x: self.normalize(x, scale), total_dict.values()))
-        graph_edge = [(class_name, word) for class_name, word_list in class_word_pool.items() for word in word_list]
+        graph_nodes_sizes = list(map(lambda x: self._normalize(x, scale), total_dict.values()))
+        graph_edge = [(class_name, word) for class_name, word_set in class_word_pool.items() for word in word_set]
 
         colors = [name for name, hexe in matplotlib.colors.cnames.items()]
         colors_json = [hexe for name, hexe in matplotlib.colors.cnames.items()]
@@ -199,7 +198,7 @@ class GraphJson:
 
         dict_node = {'nodes': [], 'edges': []}
         for node, size in total_dict.items():
-            size = self.normalize(size, scale)
+            size = self._normalize(size, scale)
             ran = np.random.randint(0, len(colors))
             dict_node['nodes'].append(
                 {'id': node, 'color': colors_json[ran], 'size': size, 'x': pos[node][0] * 1000, 'y': pos[node][1] * 1000})
@@ -220,7 +219,7 @@ def process(data, word_threshold, class_threshold):
 
     # 列表每项首个单词为对应类别，用‘ ’连接
     gj = GraphJson(data, class_column='icontitle', description_column='description')
-    with_class_list = gj.seg_and_rm_stopwords(seg_flags=['n', 'ns', 'a', 'an'], stopwords_relative_pos='lib/stopwords_biaodian.txt')
+    with_class_list = gj.seg_and_rm_stopwords(seg_flags=['n', 'a'], stopwords_relative_pos='lib/stopwords_biaodian.txt')
 
     end1 = time.time()
     excape1 = end1-start
@@ -233,15 +232,16 @@ def process(data, word_threshold, class_threshold):
     class_word_pool = gj.gen_class_word_pool(with_class_list, total_dict)
 
     end2 = time.time()
-    excape2 = end2-start
+    excape2 = end2-end1
     print('清洗花费的时间：', excape2)
 
     # 输出图的json数据
-    graph_json = gj.export_json_echarts(class_word_pool, total_dict)
+    graph_json = gj.export_json(class_word_pool, total_dict)
 
     end3 = time.time()
-    excape3 = end3-start
+    excape3 = end3-end2
     print('制图花费的时间：', excape3)
+    print('总时间：', end3-start)
 
     return graph_json
 
